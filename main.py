0574fbae5f50f69174b30b48fca6014ad7c986d2
nazeudon <nazeudon48112@icloud.com>
2021-07-15 14:37:47 +0900

    :art: Eeeenjoying fast api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 from fastapi import FastAPI
 
 
@@ -23,8 +24,44 @@ async def root():
 
 
 @app.get("/items/{item_id}/")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(
+    item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description":
+                "This is an amazing item that has a long description"
+            }
+        )
+
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description":
+                "This is an amazing item that has a long description"
+            }
+        )
+
+    return item
+
+
+@app.get("/items/")
+async def read_item2(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
 
 
 @app.get("/models/{model_name}/")
