e2f96237416b21223fcf47ec1df752c9dc0b7679
nazeudon <nazeudon48112@icloud.com>
2021-07-20 09:03:42 +0900

    :poop: little a bit tired

---
+++ main4.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+app = FastAPI()
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=status.HTTP_418_IM_A_TEAPOT,
+        content={
+            "message":
+            f"Oops! {exc.name} did something. There goes a rainbow..."
+        }
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
