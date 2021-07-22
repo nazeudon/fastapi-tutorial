aaeda9e9fbe705422f6e45d191bd42129c5d87d5
nazeudon <nazeudon48112@icloud.com>
2021-07-22 11:21:23 +0900

    :white_check_mark: finished enjoying fastapi

--- main4.py
+++ main4.py
@@ -1,5 +1,9 @@
+from typing import List, Optional
+
 from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 
 class UnicornException(Exception):
@@ -8,6 +12,35 @@ class UnicornException(Exception):
 
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items = {
+    "foo": {
+        "name": "Foo",
+        "price": 50.2
+    },
+    "bar": {
+        "name": "Bar",
+        "description": "The bartenders",
+        "price": 62,
+        "tax": 20.2
+    },
+    "baz": {
+        "name": "Baz",
+        "description": None,
+        "price": 50.2,
+        "tax": 10.5,
+        "tags": []
+    },
+}
 
 
 @app.exception_handler(UnicornException)
@@ -26,3 +59,15 @@ async def read_unicorn(name: str):
     if name == "yolo":
         raise UnicornException(name=name)
     return {"unicorn_name": name}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.put("/items/{imte_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
